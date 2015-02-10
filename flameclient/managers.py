49c86cd632d70e5e2781d1657fa347da490091f8
Cedric Brandily <zzelle@gmail.com>
2015-02-10 15:21:21 +0100

    Use nova v2 API
    Nova v1.1 API is deprecated in novaclient, let's move to nova v2 API.
    
    Change-Id: I1ed18f62c9ae98d37c40924b2e72a15b4415bbc1

--- flameclient/managers.py
+++ flameclient/managers.py
@@ -27,7 +27,7 @@ from keystoneclient.openstack.common.apiclient import (
     exceptions as keystone_exceptions)
 from keystoneclient.v2_0 import client as keystone_client
 from neutronclient.v2_0 import client as neutron_client
-from novaclient.v1_1 import client as nova_client
+from novaclient.v2 import client as nova_client
 
 
 class KeystoneManager(object):
